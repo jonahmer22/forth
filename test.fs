d8ad052356b42f59f0a1266d90a5b8084bf03d4c
jonahmer22 <jonah@jonahmerriam.net>
2026-06-10 23:12:36 -0400

    added better test for test.fs

--- test.fs
+++ test.fs
@@ -1,2 +1,21 @@
 5 10 +
-.
+. cr cr
+
+: square dup * ;
+
+create arr 10 cells allot
+
+: elem  cells arr + ;
+
+: fill-arr
+  10 0 do
+    i 10 *  i elem !
+  loop ;
+
+: print-arr
+  10 0 do
+    i elem @ . cr
+  loop ;
+
+fill-arr
+print-arr
